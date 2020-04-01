988ed9f411c41aaf4de4a17dc311f1c388953fb3
Prudhvi Ram <maddinaprudhviram@gmail.com>
2020-04-01 21:09:04 +0530

    : minor changes in search and pipe

--- src/table_details_search.pipe.ts
+++ src/table_details_search.pipe.ts
@@ -1,12 +1,10 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-  name: 'table_details_search'
+  name: "table_details_search"
 })
 export class Table_details_searchPipe implements PipeTransform {
-
   transform(items: any[], searchText: string): any[] {
-
     if (!items) {
       return [];
     }
@@ -14,10 +12,15 @@ export class Table_details_searchPipe implements PipeTransform {
       return items;
     }
     searchText = searchText.toLowerCase();
-console.log('serchtext',searchText)
+    console.log("serchtext", searchText);
 
     return items.filter(value => {
-      return (( (value.fullName.search(new RegExp(searchText, "i")) != -1))||( (value.email.search(new RegExp(searchText, "i")) != -1)))||( (value.city.search(new RegExp(searchText, "i")) != -1))||( (value.specialization.search(new RegExp(searchText, "i")) != -1));
+      return (
+        value.fullName.search(new RegExp(searchText, "i")) != -1 ||
+        value.email.search(new RegExp(searchText, "i")) != -1 ||
+        value.city.search(new RegExp(searchText, "i")) != -1 ||
+        value.specialization.search(new RegExp(searchText, "i")) != -1
+      );
     });
   }
 }
