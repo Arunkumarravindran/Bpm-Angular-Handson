fdd07c7d19166c22c7621c1677f274057092197f
Prudhvi Ram <maddinaprudhviram@gmail.com>
2020-04-02 16:41:05 +0530

    Save doctor and validation on form submit

--- src/app/get-doctor-details.service.ts
+++ src/app/get-doctor-details.service.ts
@@ -1,16 +1,19 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class GetDoctorDetailsService {
-  private_url:string="https://api.myjson.com/bins/kt8o8";
+  private_url: string = "https://api.myjson.com/bins/kt8o8";
+  saveDoctorUrl: string = "https://api.myjson.com/bins";
 
-  constructor(private http:HttpClient) { }
-  getDoctorDetails()
-  {
+  constructor(private http: HttpClient) {}
+  getDoctorDetails() {
     return this.http.get(this.private_url);
   }
+  saveDoctor(data) {
+    console.log("Save Doctor :", data);
+    this.http.post(this.saveDoctorUrl, data);
+  }
 }
- 
