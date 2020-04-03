c7d5b40a1099634bf5a3d2bf454a8dc230c9b33d
Prudhvi Ram <maddinaprudhviram@gmail.com>
2020-04-03 13:37:55 +0530

    added doctor details component and made it reusable form.

---
+++ src/components/doctor-details-form/doctor-details-form.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from "@angular/core";
+import { GetDoctorDetailsService } from "src/app/get-doctor-details.service";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
+
+@Component({
+  selector: "app-doctor-details-form",
+  templateUrl: "./doctor-details-form.component.html",
+  styleUrls: ["./doctor-details-form.component.css"]
+})
+export class DoctorDetailsFormComponent implements OnInit {
+  registerForm: FormGroup;
+  submitted = false;
+  genders$ = [{ name: "Male" }, { name: "Female" }, { name: "Others" }];
+  specialities$ = [
+    { name: "Cardiologist" },
+    { name: "Gynecology" },
+    { name: "Children health" },
+    { name: "Diabetologist" },
+    { name: "Dietician" },
+    { name: "Female" },
+    { name: "Neurology" },
+    { name: "Nephrology" },
+    { name: "Psychiatry" },
+    { name: "Vascular Surgery" }
+  ];
+  constructor(
+    private getDoctorDetailsService: GetDoctorDetailsService,
+    private modalService: NgbModal,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstName: ["", Validators.required],
+      lastName: ["", Validators.required],
+      specialization: ["", Validators.required],
+      gender: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]],
+      mobileNo: ["", [Validators.required, Validators.minLength(10)]],
+      dateOfBirth: ["", [Validators.required]],
+      practiceSince: ["", [Validators.required]],
+      city: ["", [Validators.required]]
+    });
+  }
+  get forms() {
+    return this.registerForm.controls;
+  }
+  open(content) {
+    this.modalService.open(content, { ariaLabelledBy: "modal-basic-title" });
+  }
+  save(formValues) {
+    // console.log("Details table :", formValues);
+    this.submitted = true;
+    //validation check
+    if (this.registerForm.invalid) {
+      console.log("invalid form");
+      return;
+    }
+    //any post call link..
+    this.getDoctorDetailsService.saveDoctor(formValues);
+    this.close(formValues);
+  }
+  close(content) {
+    this.modalService.dismissAll(content);
+  }
+}
