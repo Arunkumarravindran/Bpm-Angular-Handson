d2c3c93fcdaec0cb10e9cac73a87fb3b746dc9cb
user <arunkumarravindran@outlook.com>
2020-04-03 13:44:11 +0530

    json server db added

--- src/app/get-doctor-details.service.ts
+++ src/app/get-doctor-details.service.ts
@@ -6,15 +6,29 @@ import { HttpClient } from "@angular/common/http";
 })
 export class GetDoctorDetailsService {
 
-private_url:string="https://api.myjson.com/bins/dnmr4";
-  saveDoctorUrl: string = "https://api.myjson.com/bins";
+private_url:string="http://localhost:3000/doctorList";
+  saveDoctorUrl: string = "http://localhost:3000/doctorList";
+  id : Number;
 
   constructor(private http: HttpClient) {}
   getDoctorDetails() {
     return this.http.get(this.private_url);
   }
   saveDoctor(data) {
-    console.log("Save Doctor :", data);
-    this.http.post(this.saveDoctorUrl, data);
+    this.id = Math.floor(10+ Math.random()*90);
+  let saveJson = {
+    id: this.id,
+    firstName: data.firstName,
+    lastName: data.lastName,
+    mobileNo: data.mobileNumber,
+    email: data.firstName,
+    gender: data.gender,
+    practiceSince: " ",
+    specialization: data.speciality,
+    city: data.address
+  }
+
+    console.log("Save Doctor :", saveJson);
+    this.http.post(this.saveDoctorUrl, saveJson);
   }
 }
